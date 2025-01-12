3f341186c4d360048650ba3261fdd21b6322c5f1
Shifat Chowdhury <shifat.tanha.chowdhury@gmail.com>
2025-01-12 13:00:49 +0600

    Add files via upload

---
+++ Final Lab Exam/register.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shop_management";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$contact_no = $_POST['contact_no'];
+$username = $_POST['username'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO employees (name, contact_no, username, password) VALUES ('$name', '$contact_no', '$username', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New employee registered successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
